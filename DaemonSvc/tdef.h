4bc18ec63898fcd051d7886dbe466d205e5c899e
mkdym <736918023@qq.com>
2015-10-12 11:13:31 +0800

    remove no-needed defines

--- DaemonSvc/tdef.h
+++ DaemonSvc/tdef.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <string>
-#include <iostream>
-#include <stdio.h>
-#include <stdarg.h>
+//#include <iostream>
+//#include <stdio.h>
+//#include <stdarg.h>
 
 
 
@@ -12,23 +12,23 @@
 #define TSTR(quote) L##quote
 #endif
 
-#ifndef _tprintf_s
-#define _tprintf_s wprintf_s
-#endif
+//#ifndef _tprintf_s
+//#define _tprintf_s wprintf_s
+//#endif
+//
+//#ifndef _stprintf_s
+//#define _stprintf_s swprintf_s
+//#endif
+//
+//#ifndef _vsntprintf_s
+//#define _vsntprintf_s _vsnwprintf_s
+//#endif
+//
+//#ifndef _tsplitpath_s
+//#define _tsplitpath_s _wsplitpath_s
+//#endif
 
-#ifndef _stprintf_s
-#define _stprintf_s swprintf_s
-#endif
-
-#ifndef _vsntprintf_s
-#define _vsntprintf_s _vsnwprintf_s
-#endif
-
-#ifndef _tsplitpath_s
-#define _tsplitpath_s _wsplitpath_s
-#endif
-
-#define tcout std::wcout
+//#define tcout std::wcout
 typedef wchar_t tchar;
 typedef std::wstring tstring;
 
@@ -38,23 +38,23 @@ typedef std::wstring tstring;
 #define TSTR(quote) quote
 #endif
 
-#ifndef _tprintf_s
-#define _tprintf_s printf_s
-#endif
-
-#ifndef _stprintf_s
-#define _stprintf_s sprintf_s
-#endif
-
-#ifndef _vsntprintf_s
-#define _vsntprintf_s _vsnprintf_s
-#endif
-
-#ifndef _tsplitpath_s
-#define _tsplitpath_s _splitpath_s
-#endif
-
-#define tcout std::cout
+//#ifndef _tprintf_s
+//#define _tprintf_s printf_s
+//#endif
+//
+//#ifndef _stprintf_s
+//#define _stprintf_s sprintf_s
+//#endif
+//
+//#ifndef _vsntprintf_s
+//#define _vsntprintf_s _vsnprintf_s
+//#endif
+//
+//#ifndef _tsplitpath_s
+//#define _tsplitpath_s _splitpath_s
+//#endif
+//
+//#define tcout std::cout
 typedef char tchar;
 typedef std::string tstring;
 

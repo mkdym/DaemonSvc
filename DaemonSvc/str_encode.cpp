b9fdeca682e8c96c7efc994fc94fbeeb0830ac0e
mkdym <736918023@qq.com>
2015-10-15 10:57:41 +0800

    add multistr2widestr and widestr2multistr in str_encode; modify comments

--- DaemonSvc/str_encode.cpp
+++ DaemonSvc/str_encode.cpp
@@ -5,9 +5,25 @@
 #include "str_encode.h"
 
 
-//do not use log
-//because we use this function to log
+//!!!
+//all export functions in this file must not use log
+//because we use these functions to log
+
+
+static const unsigned int CP_US_ASCII = 20127;
+
+
 std::wstring ansistr2widestr(const std::string& s)
+{
+    return multistr2widestr(CP_ACP, s);
+}
+
+std::string widestr2ansistr(const std::wstring& ws)
+{
+    return widestr2multistr(CP_ACP, ws, NULL);
+}
+
+std::wstring multistr2widestr(const unsigned int from_code_page, const std::string& s)
 {
     std::wstring ws;
 
@@ -18,7 +34,7 @@ std::wstring ansistr2widestr(const std::string& s)
             break;
         }
 
-        int need_ch_len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), s.size(), NULL, 0);
+        int need_ch_len = MultiByteToWideChar(from_code_page, 0, s.c_str(), s.size(), NULL, 0);
         if (0 == need_ch_len)
         {
             print_last_err("MultiByteToWideChar fail when query need size");
@@ -28,7 +44,7 @@ std::wstring ansistr2widestr(const std::string& s)
         boost::scoped_array<wchar_t> str_buf(new wchar_t[need_ch_len]);
         memset(str_buf.get(), 0, need_ch_len * sizeof(wchar_t));
 
-        need_ch_len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), s.size(), str_buf.get(), need_ch_len);
+        need_ch_len = MultiByteToWideChar(from_code_page, 0, s.c_str(), s.size(), str_buf.get(), need_ch_len);
         if (0 == need_ch_len)
         {
             print_last_err("MultiByteToWideChar fail");
@@ -42,9 +58,7 @@ std::wstring ansistr2widestr(const std::string& s)
     return ws;
 }
 
-//do not use log
-//because we use this function to log
-std::string widestr2ansistr(const std::wstring& ws)
+std::string widestr2multistr(const unsigned int to_code_page, const std::wstring& ws, const char *default_char /*= NULL*/)
 {
     std::string s;
 
@@ -55,7 +69,7 @@ std::string widestr2ansistr(const std::wstring& ws)
             break;
         }
 
-        int need_ch_len = WideCharToMultiByte(CP_ACP, 0, ws.c_str(), ws.size(), NULL, 0, NULL, NULL);
+        int need_ch_len = WideCharToMultiByte(to_code_page, 0, ws.c_str(), ws.size(), NULL, 0, default_char, NULL);
         if (0 == need_ch_len)
         {
             print_last_err("WideCharToMultiByte fail when query need size");
@@ -65,7 +79,7 @@ std::string widestr2ansistr(const std::wstring& ws)
         boost::scoped_array<char> str_buf(new char[need_ch_len]);
         memset(str_buf.get(), 0, need_ch_len * sizeof(char));
 
-        need_ch_len = WideCharToMultiByte(CP_ACP, 0, ws.c_str(), ws.size(), str_buf.get(), need_ch_len, NULL, NULL);
+        need_ch_len = WideCharToMultiByte(to_code_page, 0, ws.c_str(), ws.size(), str_buf.get(), need_ch_len, default_char, NULL);
         if (0 == need_ch_len)
         {
             print_last_err("WideCharToMultiByte fail");
--- DaemonSvc/str_encode.h
+++ DaemonSvc/str_encode.h
@@ -13,17 +13,28 @@
 
 #else
 
-#define tstr2widestr(tstr)              ANSIStr2WideStr(tstr)
+#define tstr2widestr(tstr)              ansistr2widestr(tstr)
 #define tstr2ansistr(tstr)              tstr
 
-#define widestr2tstr(wstr)              WideStr2ANSIStr(wstr)
+#define widestr2tstr(wstr)              widestr2ansistr(wstr)
 #define ansistr2tstr(str)               str
 
 #endif
 
 
 
+//ansi-string: the current system Windows ANSI code page string. typically, gb2312 on Simplified-Chinese Windows
+//the following two functions are just implemented by multistr2widestr and widestr2multistr with code_page CP_ACP
 std::wstring ansistr2widestr(const std::string& s);
 std::string widestr2ansistr(const std::wstring& ws);
+
+
+//implemented by Windows API MultiByteToWideChar
+//if code_page is invalid, it will return empty string
+std::wstring multistr2widestr(const unsigned int from_code_page, const std::string& s);
+//default_char: when can not translate, system will use default char
+//if NULL == default_char, use system default default char(maybe is "?")
+//see Windows API WideCharToMultiByte
+std::string widestr2multistr(const unsigned int to_code_page, const std::wstring& ws, const char *default_char = NULL);
 
 
